cce1538b6e33c90ec344a5f89af7908b4c1a8173
Văn Thuấn <thuan2932000@gmail.com>
2022-03-17 04:37:00 +0700

    updata

---
+++ ASIAvinaFans/functions.php
@@ -0,0 +1,37 @@
+<?php 
+/*
+Hàm khai báo chức năng
+ */
+
+
+function asiavinafans_theme_setup(){
+		//Load textdomain thiết lập
+	load_theme_textdomain( 'asiavinafans' );
+
+		//Đăng kí menus
+	register_nav_menu('menu-header',__( 'Menu header', 'asiavinafans' ));
+
+	//Chuyển về trình soạn thảo phiên bản cũ
+	add_filter('use_block_editor_for_post', '__return_false');
+
+	//Tự động thêm title cho mỗi trang
+	add_theme_support( 'title-tag' );
+
+	//Đăng kí widgets
+	if (function_exists('register_sidebar')){
+		/**
+		 * Creates a sidebar
+		 * @param string|array  Builds Sidebar based off of 'name' and 'id' values.
+		 */
+		$args = array(
+			'name'          => __( 'search', 'asiavinafans' ),
+			'id'            => 'search',
+			'description'   => 'search box',
+			'class'         => 'search-box',
+		);
+		
+		register_sidebar( $args );
+
+	}
+}
+add_action( 'init', 'asiavinafans_theme_setup' );
---
+++ ASIAvinaFans/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="">
+  <?php wp_head() ?>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="stylesheet" href="/https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/styles.css">
+</head>
+<body>
+        <header>
+            <a href="#" ><img class="logo" src="<?php bloginfo('stylesheet_directory'); ?>/img/logo.png" alt="photo"></a>
+            <ul class="main">
+                <li><a href="#" >ASIAvina Fans</a></li>
+                <li><a href="#" >Stores</a></li>
+                <li><a href="#" >Customer Service</a></li>
+                <li><a href="#" >News</a></li>
+                <li><a href="#" >About us</a></li>
+                <li><a href="#" >Contact us</a></li>
+            </ul>
+            <div class="clearfix"></div>
+        </header>
+        <div class="background">
+            <a href="#" ><img class="ASIA" src="<?php bloginfo('stylesheet_directory'); ?>/img/logo.png" alt="photo"></a><p>Fans</p>
+            <div class="hinhanh">
+                <img class="quat1" src="<?php bloginfo('stylesheet_directory'); ?>/img/DeskFans.png" alt="">
+                <img class="quat" src="<?php bloginfo('stylesheet_directory'); ?>/img/SlideFans.png" alt="">
+                <img class="quat" src="<?php bloginfo('stylesheet_directory'); ?>/img/StandFans.png" alt="">
+                <img class="quat" src="<?php bloginfo('stylesheet_directory'); ?>/img/WallFans.png" alt="">
+                <img class="quat" src="<?php bloginfo('stylesheet_directory'); ?>/img/BoxFans.png" alt="">
+                <img class="quat" src="<?php bloginfo('stylesheet_directory'); ?>/img/CeilingFans.png" alt="">
+                <img class="quat" src="<?php bloginfo('stylesheet_directory'); ?>/img/VentilationFans.png" alt="">
+            </div>
+        </div>
+        <div class="backgroundtwo">
+            <div class="khung1">
+                <p class="thanhthoat">Thanh thoát</p>
+                <p class="vengoai">vẻ ngoài</p>
+                <p class="manhme">Mạnh mẽ</p>
+                <p class="bentrong">bên trong</p>
+                <P class="TATDIEUHOA">TẮT ĐIỀU HÒA</P>
+                <P class="BAT">BẬT</P>
+                <hr  width="25%" align="center" />
+                <a href="#" ><img class="ASIA1" src="<?php bloginfo('stylesheet_directory'); ?>/img/logo1.png" alt="photo"></a><p>Fans</p>
+            </div>
+            <div class="khung2">
+                <a href="#" ><img class="ASIA-FA" src="<?php bloginfo('stylesheet_directory'); ?>/img/ASIA-FA.png" alt="photo">
+            </div>       
+        </div>
+    </body>
+    <html>
